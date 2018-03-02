581d4d9a28a5a375a7c173677355410e2e84fddf
John Watson <jwatso95@uncc.edu>
2018-03-02 03:02:04 +0000

    done with 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,10 +4,12 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
   
-  private
-
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  # in movies_controller.rb
+  def create
+    #@movie = Movie.create!(params[:movie]) #old way
+    @movie = Movie.create!(movie_params)  # new way
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
   end
   
   def show
@@ -21,4 +23,29 @@ class MoviesController < ApplicationController
     @movie = Movie.new
     # default: render 'new' template
   end 
+  
+  def update
+    @movie = Movie.find params[:id]
+    #@movie.update_attributes!(params[:movie])  # old way
+    @movie.update_attributes!(movie_params)  # new way  
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie '#{@movie.title}' deleted."
+    redirect_to movies_path
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+    
 end
