8ca3ce33be7e9b6168ba0cb971b72cf330a1150b
John Watson <jwatso95@uncc.edu>
2018-03-02 01:56:47 +0000

    done with 4.6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,12 @@ class MoviesController < ApplicationController
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.html.haml by default
+    # will render app/views/movies/show.<extension> by default
   end
+  
+  
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
 end
